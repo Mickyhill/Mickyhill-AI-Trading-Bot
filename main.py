7879d7b6cde37908afd73122dce5c3b62ccaed6f
Mickyhill <167427481+Mickyhill@users.noreply.github.com>
2026-08-02 21:24:23 +0100

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Welcome to MickyHill AI Trading Bot")
+
+from config import BOT_NAME
+from notifier import send_message
+import bybit_api
+
+print(f"🚀 {BOT_NAME} Starting...")
+
+try:
+    response = send_message(f"🚀 {BOT_NAME} is now ONLINE!")
+    print(response)
+except Exception as e:
+    print("Telegram Error:", e)
+
+print("✅ Startup completed successfully.")
